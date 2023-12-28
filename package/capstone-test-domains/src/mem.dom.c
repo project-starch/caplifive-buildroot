204c98fa78fb5463c817ec5bb5c291e1f225f35c
Ionut Mihalache <ipopescu46@gmail.com>
2023-12-28 15:18:07 +0800

    Add C mode client get functionality.

--- package/capstone-test-domains/src/mem.dom.c
+++ package/capstone-test-domains/src/mem.dom.c
@@ -7,20 +7,61 @@
 #define __domreentry __attribute__((domreentry))
 
 #define PRINT(v) __asm__ volatile(".insn r 0x5b, 0x1, 0x43, x0, %0, x0" :: "r"(v))
+#define CAPSTONE_DPI_REGION_SHARE 0x1
 
-static unsigned ok = 0;
-static unsigned counter = 0;
-static unsigned is_server = 0;
+#define SERVER_GET 0xacef
+#define SERVER_PUT 0xaddd
+#define CLIENT_GET 0xcef
+#define CLIENT_PUT 0xcadd
+#define ACK 0xacc
+
+static unsigned *shared_region;
 
 __domentry __domreentry void mem(__domret void* ra, unsigned func, unsigned* res) {
-    if (is_server) {
-        *res = 123;
-        is_server = 0;
+    if (func == CAPSTONE_DPI_REGION_SHARE) {
+        shared_region = res;
     } else {
-        *res = 1234;
-        is_server = 1;
+        unsigned op = *shared_region;
+        op = op << 32 >> 32;
+
+        if (op == CLIENT_GET) {
+            /**
+             * The client got something from the server
+            */
+
+            /**
+             * Clear the operation
+            */
+        //    *shared_region = 0;
+
+            /**
+            * Print data from the server domain for now
+            */
+            unsigned vars_nr = *(shared_region + 4);
+            // *(shared_region + 4) = 0;
+            vars_nr = vars_nr << 32 >> 32;
+            PRINT(vars_nr);
+            unsigned i = 0;
+            while (i < vars_nr) {
+                PRINT(*(shared_region + 8 + 4 * i) << 32 >> 32);
+                // *(shared_region + 8 + 4 * i) = 0;
+                i = i + 1;
+            }
+
+            // *shared_region = ACK; /** TODO: Why is this not working and causing OOB access */
+            *res = ACK;
+            __domreturn(ra, __mem_reentry, 0);
+        }
+        if (op == SERVER_GET) {
+            /**
+             * The client should put something in the shared region for the serve to get
+            */
+           PRINT(0xdeadbeef);
+           op = 123;
+           *res = ACK;
+           __domreturn(ra, __mem_reentry, 0);
+        }
     }
 
-    *res = 123;
     __domreturn(ra, __mem_reentry, 0);
 }
