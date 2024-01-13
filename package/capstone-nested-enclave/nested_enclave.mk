ee682acaedcb3215f016cbd2969f1a1e782d1be8
Mingkai Li <mingkai.li@hotmail.com>
2024-01-13 11:17:01 +0000

    nested-enclave: structure set up

--- package/capstone-nested-enclave/nested_enclave.mk
+++ package/capstone-nested-enclave/nested_enclave.mk
@@ -4,14 +4,20 @@ CAPSTONE_NESTED_ENCLAVE_SITE_METHOD = local
 
 define CAPSTONE_NESTED_ENCLAVE_BUILD_CMDS
 	$(MAKE) -C '$(@D)'/baseline CC='$(TARGET_CC)' LD='$(TARGET_LD)'
+	# $(MAKE) -C '$(@D)'/capstone_split/cgi CC='$(TARGET_CC)' LD='$(TARGET_LD)'
+	# $(MAKE) -C '$(@D)'/capstone_split/sdom LINUX_DIR='$(LINUX_DIR)' PWD='$(@D)'/capstone_split/sdom CC='$(TARGET_CC)' LD='$(TARGET_LD)' \
+	# 	KERNEL_ARCH=$(KERNEL_ARCH) TARGET_CROSS=$(TARGET_CROSS)
 endef
 
 define CAPSTONE_NESTED_ENCLAVE_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 '$(@D)/baseline/miniweb' '$(TARGET_DIR)/nested/baseline/miniweb'
-	$(INSTALL) -D -m 0755 '$(@D)/baseline/cgi/register' '$(TARGET_DIR)/nested/baseline/cgi/register'
+	$(INSTALL) -D -m 0755 '$(@D)/baseline/cgi/register_success' '$(TARGET_DIR)/nested/baseline/cgi/register_success'
+	$(INSTALL) -D -m 0755 '$(@D)/baseline/cgi/register_fail' '$(TARGET_DIR)/nested/baseline/cgi/register_fail'
 	$(INSTALL) -D -m 0755 '$(@D)/baseline/www/index.html' '$(TARGET_DIR)/nested/baseline/www/index.html'
 	$(INSTALL) -D -m 0755 '$(@D)/baseline/404Response.txt' '$(TARGET_DIR)/nested/baseline/404Response.txt'
-	$(INSTALL) -D -m 0755 '$(@D)/debug/debug_baseline.sh' '$(TARGET_DIR)/nested/baseline/debug_baseline.sh'
+	# $(INSTALL) -D -m 0755 '$(@D)/capstone_split/cgi/cgi_register_success.dom' '$(TARGET_DIR)/nested/capstone_split/cgi_register_success.dom'
+	# $(INSTALL) -D -m 0755 '$(@D)/capstone_split/cgi/cgi_register_fail.dom' '$(TARGET_DIR)/nested/capstone_split/cgi_register_fail.dom'
+	# $(INSTALL) -D -m 0755 '$(@D)/capstone_split/miniweb_backend.smode' '$(TARGET_DIR)/nested/capstone_split/miniweb_backend.smode'
 endef
 
 $(eval $(generic-package))
