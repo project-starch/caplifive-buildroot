557904b5c7f8df40df300673f38d8b80da7853b8
Jason Yu <yuzhijingcheng1996@hotmail.com>
2024-02-04 06:43:36 +0000

    Scripts for automating nested enclave and nullblk driver case studies added

---
+++ scripts/capstone_nested_baseline.tcl
@@ -0,0 +1,24 @@
+set timeout -1
+
+cd $::env(CAPSTONE_QEMU_PATH)
+
+spawn sh start.sh
+
+interact {
+    -o "login: " {
+        send "root\r"
+        return
+    }
+}
+
+interact {
+    -o "# " {
+        send "cd /nested/baseline\r"
+        send "./miniweb &\r"
+        send "sleep 1\r"
+        send "busybox wget -O - http://localhost:8888/index.html\r"
+        send "busybox wget -O - http://localhost:8888/null.html\r"
+        send "busybox wget --post-data \"name=Alex&email=alex@email.com\" -O - http://localhost:8888/cgi/register\r"
+        send "poweroff -f\r"
+    }
+}
