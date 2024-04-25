31adef34ce0dd2081391ce8ff7d00e4f8075a9be
Mingkai Li <mingkai.li@hotmail.com>
2024-04-25 09:12:23 +0000

    DPDK: benchmark and benchmark script added

---
+++ scripts/benchmark_dpdk.tcl
@@ -0,0 +1,30 @@
+set timeout -1
+
+cd $::env(CAPSTONE_QEMU_PATH)
+
+spawn sh start.sh
+
+interact {
+    -o "login: " {
+        send "root\r"
+        return
+    }
+}
+
+interact {
+    -o "# " {
+        send "insmod /capstone.ko\r"
+        send "mkdir -p /mnt/huge\r"
+        send "mount -t hugetlbfs nodev /mnt/huge\r"
+        send "echo 2048 > /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages\r"
+        send "/dpdk-multip/dpdk-multip_server -l 0 -n 4 -- -p 3 -n 2 < /benchmark/dpdk_multip_commands\r"
+        return
+    }
+}
+
+interact {
+    -o "# " {
+        send "/print-counters\r"
+        send "poweroff -f\r"
+    }
+}
